afaa38e15626dd2549d960b927b00b463453375b
Jonatas Baldin <jonatas.baldin@gmail.com>
2017-10-29 14:38:47 -0200

    Duplicate routes to use on Heroku (without NGINX) – not the best fix, I know

--- wsquiz/routing.py
+++ wsquiz/routing.py
@@ -11,6 +11,9 @@ from quiz.consumers import (
 channel_routing = [
     route('websocket.connect', top_players_connect, path=r'^/top_players/$'),
     route('websocket.disconnect', top_players_disconnect, path=r'^/top_players/$'),
+    # This routes are duplicated to be used in Heroku, without nginx
+    route('websocket.connect', top_players_connect, path=r'^/ws/top_players/$'),
+    route('websocket.disconnect', top_players_disconnect, path=r'^/ws/top_players/$'),
     route('websocket.connect', ws_connect),
     route('websocket.receive', ws_message),
     route('websocket.disconnect', ws_disconnect),
